eeaf73571d70e1d972e560140f678c8dfce0449d
Friedland, Peter <peter.friedland@partner.comparex.com>
2017-03-30 11:28:41 +0200

    ported project to monogame 3.6

---
+++ DevmaniaGame/States/MainGame.cs
@@ -0,0 +1,97 @@
+﻿
+using Game2DFramework.Sound;
+using Game2DFramework.States;
+using Game2DFramework.States.Transitions;
+using Microsoft.Xna.Framework;
+using DevmaniaGame.Objects;
+using Microsoft.Xna.Framework.Media;
+
+namespace DevmaniaGame.States
+{
+    class MainGame : InitializableState
+    {
+        private ExplosionManager _explosionManager;
+        private GameBackground _gameBackground;
+        private Player _player;
+        private CarManager _carManager;
+        private Weather _weather;
+        public float _globalTime;
+        private float _waitTimer;
+
+        protected override void OnEntered(object enterInformation)
+        {
+            SoundService.PlaySong("MainTheme", true);
+        }
+
+        protected override void OnInitialize(object enterInformation)
+        {
+            _globalTime = 0f;
+            SoundService.RegisterSong("MainTheme", Game.Content.Load<Song>("sounds/hardebm"));
+
+            _explosionManager = Game.RegisterGlobalObject("ExplosionManager", new ExplosionManager(Game));
+
+            _player = Game.RegisterGlobalObject("Player",  new Player(Game, new Vector2(500f, 400f)));
+            _carManager = new CarManager(Game);
+            _weather = new Weather(Game);
+            _player.weather = _weather;
+            Game.DepthRenderer.Register(_player);
+            _gameBackground = new GameBackground(Game);
+            _gameBackground.SetAnimationTime(0.25f);
+        }
+
+        public override void OnLeave()
+        {
+            
+        }
+
+        private bool _playerIsDead = false;
+
+        public override StateChangeInformation OnUpdate(float elapsedTime)
+        {
+            _globalTime += elapsedTime;
+
+            _explosionManager.Update(elapsedTime);
+            _gameBackground.Update(elapsedTime);
+            _carManager.Update(elapsedTime);
+            _player.Update(elapsedTime);
+            _weather.Update(elapsedTime);
+
+            if (_playerIsDead)
+            {
+                _waitTimer += elapsedTime;
+                if (_waitTimer >= 3.0f)
+                {
+                    return StateChangeInformation.StateChange(typeof (GameOverState), typeof (FlipTransition),
+                                                              _player.Points);
+                }
+            }
+            else if (!_playerIsDead && _player.PlayerState == PlayerState.Dead)
+            {
+                SoundService.StopCurrentSong();
+                _playerIsDead = true;
+                _waitTimer = 0;
+            }
+
+            _carManager.spawnTime = (100 - _player.Points/50);
+            if (_carManager.spawnTime <= 10)
+                _carManager.spawnTime = 10;
+            _carManager.points = _player.Points + 150;
+
+            return StateChangeInformation.Empty;
+        }
+
+        public override void OnDraw(float elapsedTime)
+        {
+            _gameBackground.Draw();
+            _weather.Draw(elapsedTime);
+            Game.DepthRenderer.Draw(Game.SpriteBatch);
+
+            _explosionManager.Draw();
+
+            Game.ShapeRenderer.DrawFilledRectangle(0, 0, 800, 600, Color.White * _weather._sun._blendVal);
+            _weather._cloud.RenderRainOnTop(elapsedTime);
+
+            //Game.ShapeRenderer.DrawFilledRectangle(0, 0, 800, 600, Color.Gray * 0.5f);
+        }
+    }
+}
--- Game2DFramework/Properties/AssemblyInfo.cs
+++ Game2DFramework/Properties/AssemblyInfo.cs
@@ -6,23 +6,21 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Game2DFramework")]
-[assembly: AssemblyProduct("Game2DFramework")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Game2DFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("5ac04534-f9e7-4686-ae09-0159c14e7bda")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9021333c-d2b6-4b8b-a699-743445260bd6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
